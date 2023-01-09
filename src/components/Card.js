d66035d86a821b0c161cfadaf6361e2bf74fe91a
ARTALEAL <artaleal@yandex.ru>
2023-01-09 19:19:58 +0300

    develop: next step likes & delete

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,25 @@
+import React, { useContext } from 'react';
+import { CurrentUserContext } from '../contexts/CurrentUserContext';
+
 function Card({ card, onCardClick }) {
+  const currentUser = useContext(CurrentUserContext);
+
+  // Определяем, являемся ли мы владельцем текущей карточки
+  const isOwn = card.owner._id === currentUser._id;
+
+  // Далее в разметке используем переменную для условного рендеринга
+  {
+    isOwn && <button className="button_del" onClick={handleDeleteClick} />;
+  }
+
+  // Определяем, есть ли у карточки лайк, поставленный текущим пользователем
+  const isLiked = card.likes.some((i) => i._id === currentUser._id);
+
+  // Создаём переменную, которую после зададим в `className` для кнопки лайка
+  const cardLikeButtonClassName = `element__like-button ${
+    isLiked && 'element__like-button_active'
+  }`;
+
   function handleCardClick() {
     onCardClick(card);
   }
